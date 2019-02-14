5186327c0827b6cc2a6f770728c7ba4f34ebda4c
enyes_fang <enyes_fang@163.com>
2019-02-15 00:16:06 +0800

    init

--- sentinel-client/src/main/java/org/enyes/sentinel/client/Main.java
+++ sentinel-client/src/main/java/org/enyes/sentinel/client/Main.java
@@ -1,9 +1,46 @@
 package org.enyes.sentinel.client;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("aaaa");
+        // 配置规则.
+        initFlowRules();
+
+        while (true) {
+            Entry entry = null;
+            try {
+                entry = SphU.entry("HelloWorld");
+                // 资源中的逻辑.
+                System.out.println("hello world");
+            } catch (BlockException e1) {
+                System.out.println("blocked!");
+            } finally {
+                if (entry != null) {
+                    entry.exit();
+                }
+            }
+        }
+    }
+
+    private static void initFlowRules(){
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("HelloWorld");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(20);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
     }
 
 }
